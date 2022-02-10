2fd17d837fbdbdda21b08ca0a0eacdfd5e7c3dc5
BunRum <adamventura4@gmail.com>
2022-02-09 20:10:02 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 var games = {
-    "klona": "https://media.githubusercontent.com/media/BunRum/jspsx/main/klona.img"
+    "klona": "https://media.githubusercontent.com/media/BunRum/jspsx/main/klona.img",
+    "mgs": "https://media.githubusercontent.com/media/BunRum/jspsx/main/mgs.bin"
 }
