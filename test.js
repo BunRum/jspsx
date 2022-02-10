b2e8c4e15665ad2cfac97d8d1de5bc9e789a8171
BunRum <adamventura4@gmail.com>
2022-02-09 19:21:31 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var games = {
-    "klona": "Klonoa - Door to Phantomile [U] [SLUS-00585].img"
+    "klona": "https://github.com/BunRum/jspsx/raw/main/Klonoa%20-%20Door%20to%20Phantomile%20%5BU%5D%20%5BSLUS-00585%5D.img"
 }
